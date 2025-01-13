545bb11e44397e457ca6cd2ceba739aecb3c3e50
Joel S Mathews <159769692+joelsmathewss@users.noreply.github.com>
2025-01-14 03:33:41 +0530

    ArithmeticOperators.py

--- Introduction/ArithmeticOperators.py
+++ Introduction/ArithmeticOperators.py
@@ -1,11 +1,3 @@
-"""
-Title     : Arithmetic Operators
-Subdomain : Introduction
-Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 April 2020
-Problem   : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
-"""
 if __name__ == "__main__":
     a = int(input())
     b = int(input())
