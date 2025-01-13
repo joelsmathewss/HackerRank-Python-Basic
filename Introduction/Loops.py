cbb9d8b81c8650ce43012227795ae3501d509383
Joel S Mathews <159769692+joelsmathewss@users.noreply.github.com>
2025-01-14 03:32:58 +0530

    Loops.py

--- Introduction/Loops.py
+++ Introduction/Loops.py
@@ -1,11 +1,3 @@
-"""
-Title     : Loops
-Subdomain : Introduction
-Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 13 May 2020
-Problem   : https://www.hackerrank.com/challenges/python-loops/problem
-"""
 if __name__ == "__main__":
     n = int(input())
     for i in range(n):
