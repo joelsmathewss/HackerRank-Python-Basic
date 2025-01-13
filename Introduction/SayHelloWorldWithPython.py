0397a636b8afcb44f54eadc3ba662720703145c9
Joel S Mathews <159769692+joelsmathewss@users.noreply.github.com>
2025-01-14 03:30:14 +0530

    SayHelloWorldWithPython.py

--- Introduction/SayHelloWorldWithPython.py
+++ Introduction/SayHelloWorldWithPython.py
@@ -1,9 +1,2 @@
-"""
-Title     : Say Hello, World! With Python
-Subdomain : Introduction
-Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 23 July 2018
-Problem   : https://www.hackerrank.com/challenges/py-hello-world/problem
-"""
-print("Hello, World!")
+my_string = "Hello, World!"
+print(my_string)
